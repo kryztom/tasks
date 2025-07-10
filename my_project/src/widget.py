9a6e27d83db699b8bdbe643f4a0aaa80bb5b158c
Kryztom <kryztom@bk.ru>
2025-07-10 17:18:11 +0300

    repository tasks

--- my_project/src/widget.py
+++ my_project/src/widget.py
@@ -1,24 +1,24 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(account_card: str) -> str:
-    '''Функция, которая обрабатывет информацию как о картах,
-    так и о счетах '''
+    """Функция, которая обрабатывет информацию как о картах,
+    так и о счетах"""
     account_card_list = []
     account_card_split = account_card.split()
 
     for word in account_card_split:
         if word.isalpha():
             account_card_list.append(word)
-    card_type = ''.join(account_card_list)
+    card_type = "".join(account_card_list)
 
     number_card = account_card_split[-1]
     if account_card_split[0].lower() == "счет":
 
-        return f'{card_type} {get_mask_account(number_card)}'
+        return f"{card_type} {get_mask_account(number_card)}"
     else:
 
-        return f'{card_type} {get_mask_card_number(number_card)}'
+        return f"{card_type} {get_mask_card_number(number_card)}"
 
 
 # Примеры входных данных для проверки функции
@@ -34,14 +34,14 @@ def mask_account_card(account_card: str) -> str:
 # Проверка работы кода
 if __name__ == "__main__":
     for data in [
-        'Maestro 1596837868705199',
-        'Счет 64686473678894779589',
-        'MasterCard 7158300734726758',
-        'Счет 35383033474447895560',
-        'Visa Classic 6831982476737658',
-        'Visa Platinum 8990922113665229',
-        'Visa Gold 5999414228426353',
-        'Счет 73654108430135874305'
+        "Maestro 1596837868705199",
+        "Счет 64686473678894779589",
+        "MasterCard 7158300734726758",
+        "Счет 35383033474447895560",
+        "Visa Classic 6831982476737658",
+        "Visa Platinum 8990922113665229",
+        "Visa Gold 5999414228426353",
+        "Счет 73654108430135874305",
     ]:
         print(mask_account_card(data))
 
@@ -49,15 +49,15 @@ if __name__ == "__main__":
 def get_date(date: str) -> str:
     """Функция редактирования даты"""
 
-    date_split = date.split('-')
+    date_split = date.split("-")
     date_day = date_split[2][:2]
     date_mouth = date_split[:2]
-    date_reverse = '.'.join(date_mouth[::-1])
-    new_date = f'{date_day}.{date_reverse}'
+    date_reverse = ".".join(date_mouth[::-1])
+    new_date = f"{date_day}.{date_reverse}"
 
     return new_date
 
 
+# Проверка функции
 if __name__ == "__main__":
     print(get_date("2024-03-11T02:26:18.671407"))
-
